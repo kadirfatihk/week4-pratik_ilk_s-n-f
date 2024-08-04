f3315668cb4f9a3d52398646057c252d86cf73c3
Kadir Fatih KÖSE <101864021+kadirfatihk@users.noreply.github.com>
2024-08-04 18:29:46 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week4_pratik_ilk_sınıf
+{
+    public class Person
+    {
+        private string name;
+        private string surname;
+        private int birthday;
+
+        // isim tanımlama
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        // soyisim tanımlama
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+
+        // doğum tarihi tanımlama
+        public int Birthday
+        {
+            get
+            {
+                return birthday;
+            }
+            set
+            {
+                if (value >= 1940 && value <=2017)
+                    birthday = value;
+            }
+        }
+        // öğretmen bilgilerini yazdırma metodu
+        public void TeacherDisplay()
+        {
+            Console.WriteLine($"Öğretmen: {name} {surname} {birthday}");
+        }
+
+        // öğrenci bilgilerini yazdırma metodu
+        public void StudentDisplay()
+        {
+            Console.WriteLine($"Öğrenci: {name} {surname} {birthday}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace week4_pratik_ilk_sınıf
+{
+    public class Program
+    {
+        public static void Main (string[] args)
+        {
+            // öğretmen tanımlandı.
+            Person teacher = new Person();
+            teacher.Name = "Jose";
+            teacher.Surname = "Mourinho";
+            teacher.Birthday = 1963;
+
+            teacher.TeacherDisplay();
+
+            // öğrenci-1 tanımlandı.
+            Person student_1 = new Person();
+            student_1.Name = "İrfan Can";
+            student_1.Surname = "Kahveci";
+            student_1.Birthday = 1995;
+
+            // öğrenci-2 tanımlandı.
+            Person student_2 = new Person();
+            student_2.Name = "Dusan";
+            student_2.Surname = "Tadic";
+            student_2.Birthday = 1988;
+
+            // öğrenci-3 tanımlandı.
+            Person student_3 = new Person();
+            student_3.Name = "Ferdi";
+            student_3.Surname = "Kadıoğlu";
+            student_3.Birthday = 1999;
+
+            student_1.StudentDisplay();
+            student_2.StudentDisplay();
+            student_3.StudentDisplay();
+        }
+    }
+}
